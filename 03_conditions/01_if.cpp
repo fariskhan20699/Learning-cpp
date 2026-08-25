8143e3f7013fe98d020204afb584e66386434024
Muhammad Faris Khan <khangee90870@gmail.com>
2026-08-25 10:38:36 +0000

    adding condition folder and file

---
+++ 03_conditions/01_if.cpp
@@ -0,0 +1,46 @@
+/*
+    if Statement:
+    - Used to make decisions in a program.
+    - The code inside if runs only when the condition is true.
+
+    Syntax:
+    
+    if (condition) {
+        // code to execute
+    }
+*/
+/////////////////////////////////////////////////////////////////
+#include <iostream>
+using namespace std;
+int main() {
+
+    int age = 18;
+
+    // The if statement executes a block of code
+    // only when the condition is true.
+
+    if (age >= 18){
+        cout<<"You are eligible."<<endl;
+    }
+    // Guess the Output -> if(age >= 18);
+    return 0;
+}
+//////////////////////////////////////////////////////////////////
+
+/*
+//////////
+//Output//
+/////////
+
+You are eligible
+
+*/
+/////////////////////////////////////////////////////////////////
+
+/*
+Practice Task:
+Create a program that takes a number from 
+the user and prints "Positive number" only 
+if the number is greater than 0.
+*/
+////////////////////////////////////////////////////////////////
---
+++ 03_conditions/02_if_else.cpp
@@ -0,0 +1,53 @@
+/*
+    if/else Statement:
+
+    -> if runs when the condition is true.
+    -> else runs when the condition is false.
+    -> It provides two possible paths.
+
+    Syntax:
+    if (condition){
+        // code if condition is true
+    }
+
+    else{
+        // code if condition is false
+    }
+*/
+/////////////////////////////////////////////////////////////////////
+#include <iostream>
+using namespace std;
+int main() {
+
+    int age = 17;
+
+    /*
+       if/else is used to choose between two conditions.
+       If the condition is true, the if block runs.
+       Otherwise, the else block runs.
+    */
+
+    if (age >= 18){
+        cout<<"You are an adult."<<endl;
+    }
+    else{
+        cout<<"You are not an adult."<<endl;
+    }
+
+    return 0;
+}
+//////////////////////////////////////////////////////////////////////
+/*
+//////////
+//Output//
+//////////
+
+You are not an adult.
+
+*/
+//////////////////////////////////////////////////////////////////////
+
+/*
+Practice Task: 
+Take a number from the user and check whether it is even or odd using if/else.
+*/
